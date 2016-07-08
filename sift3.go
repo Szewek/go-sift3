4b61465aba4ec55547d6e0287310e141b7f276c1
Szewek <shevkpl@gmail.com>
2016-07-08 18:33:43 +0200

    Documented maxOffset

--- sift3.go
+++ sift3.go
@@ -1,8 +1,15 @@
+// Package sift3 contains Sift3 (a "Super Fast and Accurate string distance algorithm") implementation
+// For more details, see http://siderite.blogspot.com/2007/04/super-fast-and-accurate-string-distance.html
 package sift3
+
+// DefaultOffset
+const DefaultOffset int = 5
 
 // SiftBytes measures distance between two arrays of bytes.
 // Use this function if provided strings aren't Unicode (e-mails, URLs, etc...).
-func SiftBytes(a, b []byte, o int) float64 {
+//
+// maxOffset represents a range where values are swapped. By default it should be set to "sift3.DefaultOffset".
+func SiftBytes(a, b []byte, maxOffset int) float64 {
 	la := len(a)
 	lb := len(b)
 	if la == 0 {
@@ -16,7 +23,7 @@ func SiftBytes(a, b []byte, o int) float64 {
 		if a[ca] == b[cb] {
 			lcs++
 		} else {
-			for i := 1; i < o; i++ {
+			for i := 1; i < maxOffset; i++ {
 				if ca+i < la && a[ca+i] == b[cb] {
 					ca += i
 					break
@@ -35,7 +42,9 @@ func SiftBytes(a, b []byte, o int) float64 {
 
 // SiftRunes measures distance between two arrays of runes.
 // Use this function if strings contain Unicode symbols.
-func SiftRunes(a, b []rune, o int) float64 {
+//
+// maxOffset represents a range where values are swapped. By default it should be set to "sift3.DefaultOffset".
+func SiftRunes(a, b []rune, maxOffset int) float64 {
 	la := len(a)
 	lb := len(b)
 	if la == 0 {
@@ -49,7 +58,7 @@ func SiftRunes(a, b []rune, o int) float64 {
 		if a[ca] == b[cb] {
 			lcs++
 		} else {
-			for i := 1; i < o; i++ {
+			for i := 1; i < maxOffset; i++ {
 				if ca+i < la && a[ca+i] == b[cb] {
 					ca += i
 					break
